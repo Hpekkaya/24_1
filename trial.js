e76314c8382dc51fc80c1da70686dfe5363ecb7e
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-17 00:24:31 +0300

    Math pbjects tried

--- trial.js
+++ trial.js
@@ -1,4 +1,16 @@
 
-
+//math object
+console.log("The most close value  4.3-> ", 
+Math.round(4.3), " and and (4.55)-> ", Math.round(4.55)) //en yakın değer
+console.log("Rounded up to its nearest integer 4.3-> ",Math.ceil(4.3)) //üste yuvarla
+console.log("Rounded down to its nearest integer  4.9-> ",Math.floor(4.9)) //alt sayıya yuvarla
+console.log("Returns the integer part  4.55-> ",Math.trunc(4.55)) //tam sayı kısmı
+console.log("Returns the value of 2 to the power of 8-> ", Math.pow(8,2)) //üssü
+console.log("Returns the squarroot of 64-> ",Math.sqrt(64)) 
+console.log("Find the lowest  value in (0,34,89,146) : ", Math.min(0,34,89,146)) 
+console.log("Find the highest  value in (0,34,89,146) : ", Math.max(0,34,89,146)) 
+console.log("Returns a random number bet  0-100 :", (Math.random()*100).toFixed(0))
+console.log("Returns a random number bet  0-100 :", Math.trunc(Math.random()*100))
+console.log("Returns the value of 1/4 to the power of 16->", Math.pow(16,1/4))
 
 
