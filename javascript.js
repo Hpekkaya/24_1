bb232236de7ab62b9901810e2fadbb248c94cbfc
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-17 09:55:14 +0300

    Comparison tried

---
+++ javascript.js
@@ -0,0 +1,41 @@
+//date object 
+// const myDate = new Date()
+// console.log("Full Date :", myDate)
+// console.log("Full Year :", myDate.getFullYear())
+// console.log("Only Month :", myDate.getMonth()) //ocak 0 değerinden başlar
+// console.log("Day of the week :", myDate.getDay()) //pazar 0 değerinden başlıyor
+// console.log("Day of the Month :", myDate.getDate())
+// console.log("Only Hour :", myDate.getHours())
+// console.log("Only Hour :", myDate.getMinutes())
+// console.log("Only Hour :", myDate.getSeconds())
+// console.log("Previous Full Date  :", new Date("May 11 1970 05:10:"))
+
+//math object
+// console.log("The most close value  4.3-> ", 
+// Math.round(4.3), " and and (4.55)-> ", Math.round(4.55)) 
+// console.log("Rounded up to its nearest integer 4.3-> ",Math.ceil(4.3)) 
+// console.log("Rounded down to its nearest integer  4.9-> ",Math.floor(4.9)) 
+// console.log("Returns the integer part  4.55-> ",Math.trunc(4.55)) //tam sayı kısmı
+// console.log("Returns the value of 2 to the power of 8-> ", Math.pow(8,2)) 
+// console.log("Returns the squarroot of 64-> ",Math.sqrt(64)) 
+// console.log("Find the lowest  value in (0,34,89,146) : ", Math.min(0,34,89,146)) 
+// console.log("Find the highest  value in (0,34,89,146) : ", Math.max(0,34,89,146)) 
+// console.log("Returns a random number bet  0-100 :", (Math.random()*100).toFixed(0))
+// console.log("Returns a random number bet  0-100 :", Math.trunc(Math.random()*100))
+// console.log("Returns the value of 1/4 to the power of 16->", Math.pow(16,1/4))
+
+
+//comparison (karşılaştırma)
+// let x = 8 ;
+// let y = 20;
+// console.log(" '==' '!=' compare only value \"8\" != x -> ")
+// console.log( "8" != x, "There is no type precision ") 
+
+// console.log(" '===' '!==' compare value  and type \"8\" != x -> ")
+// console.log("\"8\" === x -> ", "8" === x, "--Compare both value and type ") 
+// console.log("\"8\" !== x -> ", "8" !== x, "--Compare both value and type ") 
+
+// console.log(!x)
+
+
+
